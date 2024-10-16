e2800a367c13e3cd83062baaac6b64674600c3d5
Serhii Borysenko <borisenko37@gmail.com>
2024-10-16 11:35:59 +0200

    add contacts to bd

---
+++ src/db/contacts.js
@@ -0,0 +1,38 @@
+import mongoose from 'mongoose';
+
+const contactSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+    },
+    isFavourite: {
+      type: Boolean,
+      default: false,
+    },
+    contactType: {
+      type: String,
+      enum: ['work', 'home', 'personal'],
+      required: true,
+      default: 'personal',
+    },
+  },
+  { timestamps: true },
+);
+
+const Contact = mongoose.model('Contact', contactSchema);
+
+export default Contact;
+
+// name - string, required
+// phoneNumber - string, required
+// email - string
+// isFavourite - boolean, default false
+// contactType - string, enum(’work’, ‘home’, ‘personal’), required, default ‘personal’
--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import pinoHttp from 'pino-http';
 
+import { getAllContacts, getContactByID } from './services/contacts.js';
+
 import { env } from './utils/env.js';
 
 const PORT = Number(env('PORT', '3000'));
@@ -19,13 +21,39 @@ const setupServer = () => {
     }),
   );
 
-  app.use((req, res, next) => {
-    console.log(req.query);
-    next();
+  app.get('/contacts', async (req, res) => {
+    try {
+      const contacts = await getAllContacts();
+      res.json({
+        status: 200,
+        message: 'Successfully found contacts!',
+        data: contacts,
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Internal Server Error');
+    }
   });
 
-  app.get('/', (req, res) => {
-    res.send('Hello Node.js');
+  app.get('/contacts/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+      const contact = await getContactByID(id);
+
+      if (contact === null) {
+        return res.status(404).json({
+          message: 'Contact not found',
+        });
+      }
+      res.json({
+        status: 200,
+        message: `Successfully found contact with id ${id}!`,
+        data: contact,
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Internal Server Error');
+    }
   });
 
   app.use((req, res, next) => {
