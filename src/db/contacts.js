8edd1d81b2653b7ae51fe643f43f0dff4d3279bd
Serhii Borysenko <borisenko37@gmail.com>
2024-10-17 10:04:42 +0200

    add branch

--- src/db/contacts.js
+++ src/db/contacts.js
@@ -30,9 +30,3 @@ const contactSchema = new mongoose.Schema(
 const Contact = mongoose.model('Contact', contactSchema);
 
 export default Contact;
-
-// name - string, required
-// phoneNumber - string, required
-// email - string
-// isFavourite - boolean, default false
-// contactType - string, enum(’work’, ‘home’, ‘personal’), required, default ‘personal’
