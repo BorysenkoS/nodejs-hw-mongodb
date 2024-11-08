6546ac35ec77c63eb42809924856be69bca46ef8
Serhii Borysenko <borisenko37@gmail.com>
2024-11-08 14:56:16 +0100

    logout

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -8,6 +8,7 @@ import { validateBody } from '../middlewares/validateBody.js';
 
 import { loginUserSchema } from '../validation/auth.js';
 import { loginUserController } from '../controllers/auth.js';
+import { logoutUserController } from '../controllers/auth.js';
 
 const router = Router();
 
@@ -24,3 +25,5 @@ router.post(
   validateBody(loginUserSchema),
   ctrlWrapper(loginUserController),
 );
+
+router.post('/logout', ctrlWrapper(logoutUserController));
