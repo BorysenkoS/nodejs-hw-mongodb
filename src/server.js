c8f34f082b8ce89607f599048b748609cffbbf22
Serhii Borysenko <borisenko37@gmail.com>
2024-11-08 14:46:20 +0100

    cookies

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ import cors from 'cors';
 import pinoHttp from 'pino-http';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { errorHandler } from './middlewares/errorHanler.js';
-
+import cookieParser from 'cookie-parser';
 import router from './routers/index.js';
 
 import { env } from './utils/env.js';
@@ -13,8 +13,10 @@ const PORT = Number(env('PORT', '3000'));
 const setupServer = () => {
   const app = express();
 
+  app.use(express.json);
   app.use(router);
   app.use(cors());
+  app.use(cookieParser());
   app.use(
     pinoHttp({
       transport: {
