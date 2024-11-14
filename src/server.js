a57e2c35b7427e248854b22b6d0dc24502b7963c
Serhii Borysenko <borisenko37@gmail.com>
2024-11-14 12:59:37 +0100

    docs

--- src/server.js
+++ src/server.js
@@ -8,6 +8,7 @@ import router from './routers/index.js';
 
 import { env } from './utils/env.js';
 import { UPLOAD_DIR } from './constants/index.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 const PORT = Number(env('PORT', '3000'));
 
@@ -17,6 +18,7 @@ const setupServer = () => {
   app.use(cors());
   app.use(cookieParser());
   app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());
 
   app.use(
     pinoHttp({
